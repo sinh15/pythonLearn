47d5070b0084813ee7c9415d9774772f6918bbad
sinh15 <sinh15@users.noreply.github.com>
2016-02-17 11:54:25 +0100

    add files

--- _own/pathAdder/pathAdder.py
+++ _own/pathAdder/pathAdder.py
@@ -4,14 +4,14 @@ from __future__ import print_function
 # Read files and create if it doesnt exist
 fname = raw_input("Enter file name: ")
 oname = raw_input("Select desired output name: ")
-extraPath = raw_input("Enter the desired extra path (HIT ENTER if you want to use the one in the code): ")
+extraPath = raw_input("Enter the 'extra path' (HIT ENTER to use default): ")
 text = open(fname, 'U')
 output = open(oname, 'w+')
 
 # ================================================== #
 # Introduce on the next var the value you want added #
 # ================================================== #
-extraPathCode = "PATH_ADDED_FOR_REFERENCE_"
+extraPathCode = "images/onefront/destinations/"
 # ================================================== #
 
 # Select which extra path should be Used
